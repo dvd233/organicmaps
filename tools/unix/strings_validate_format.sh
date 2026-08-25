674eb650eb112be6833f21a18f5730bbb7219860
S. Kozyr <s.trump@gmail.com>
2026-08-25 10:06:14 +0300

    Dropped unused iphone/plist.txt
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- tools/unix/strings_validate_format.sh
+++ tools/unix/strings_validate_format.sh
@@ -10,7 +10,7 @@ OMIM_PATH=$(realpath $OMIM_PATH)
 echo
 echo "Validating translation string files format"
 
-for f in data/strings/strings.txt data/strings/types_strings.txt data/strings/sound.txt iphone/plist.txt; do
+for f in data/strings/strings.txt data/strings/types_strings.txt data/strings/sound.txt; do
   echo ">> Validating '$f' ..."
   python3 "$OMIM_PATH/tools/python/twine/python_twine/twine_cli.py" validate-twine-file $OMIM_PATH/$f
 done
